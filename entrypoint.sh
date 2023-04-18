0195338de701f8e7400c07d3286f32c90ecb4164
saurav <saurav@va2pt.com>
2023-04-18 15:58:29 +0530

    change user

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-mkdir /deployment/.ssh
-echo "$PRIVATE_KEY" > /deployment/.ssh/id_rsa # works even if key is not rsa kind
-chmod 0600 /deployment/.ssh/id_rsa
+mkdir /home/deployment/.ssh
+echo "$PRIVATE_KEY" > /home/deployment/.ssh/id_rsa # works even if key is not rsa kind
+chmod 0600 /home/deployment/.ssh/id_rsa
 
 if [ -z "${HOST_FINGERPRINT}" ]; then
     echo ">> No public ssh fingerprint found, man-in-the-middle protection disabled!"
-    ssh-keyscan -H -p ${PORT} ${HOST} > /deployment/.ssh/known_hosts
+    ssh-keyscan -H -p ${PORT} ${HOST} > /home/deployment/.ssh/known_hosts
 else
     echo ">> Public ssh fingerprint found, man-in-the-middle protection enabled."
-    echo "${HOST_FINGERPRINT}" > /deployment/.ssh/known_hosts
+    echo "${HOST_FINGERPRINT}" > /home/deployment/.ssh/known_hosts
 fi
 
 # Execute the command
